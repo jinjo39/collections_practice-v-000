a5228b213cbecfbd360f1dc5c772003c43a0fcf3
Christina Delavar <c.delavar@gmail.com>
2018-09-15 17:28:59 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,13 +3,5 @@ def sort_array_asc(array)
 end
 
 def sort_array_desc(array)
-  array.sort do |a,b|
-    if a == b
-      0
-    elsif a < b
-      1
-    elsif a > b
-      -1
-    end
-  end
+  array.sort {|a,b| b <=> a}
 end
